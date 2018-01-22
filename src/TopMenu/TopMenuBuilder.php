cabe4383c54ad1d845a08f01f520c96aa67c0a37
Ryan Owens <ryanvader82@gmail.com>
2018-01-22 10:48:45 -0600

    Switching to GAPI v2

--- src/TopMenu/TopMenuBuilder.php
+++ src/TopMenu/TopMenuBuilder.php
@@ -35,27 +35,18 @@ use CosmoQuestMenuBuilder\MenuItem;
             }
             $html .= "<li class='item-search'>
                     <div class='search'>
-                      <div id='cse' style='width: 100%;'>Loading</div>
-                        <script src='//www.google.com/jsapi' type='text/javascript'></script>
-                        <script type='text/javascript'>
-                        google.load('search', '1', {language: 'en', style: src='css/gcse.css'});
-                        google.setOnLoadCallback(function() {
-                        var customSearchOptions = {};
-                        var orderByOptions = {};
-                        orderByOptions['keys'] = [{label: 'Relevance', key: ''} , {label: 'Date', key: 'date'}];
-                        customSearchOptions['enableOrderBy'] = true;
-                        customSearchOptions['orderByOptions'] = orderByOptions;
-                        var imageSearchOptions = {};
-                        imageSearchOptions['layout'] = 'google.search.ImageSearch.LAYOUT_POPUP';
-                        customSearchOptions['enableImageSearch'] = true;
-                        customSearchOptions['overlayResults'] = true;
-                        var customSearchControl =   new google.search.CustomSearchControl('004401904339917838776:3zrwgrnv-hw', customSearchOptions);
-                        customSearchControl.setResultSetSize(google.search.Search.FILTERED_CSE_RESULTSET);
-                        var options = new google.search.DrawOptions();
-                        options.setAutoComplete(true);
-                        customSearchControl.draw('cse', options);
-                        }, true);
-                        </script>
+                    <script>
+                        (function() {
+                          var cx = '004401904339917838776:3zrwgrnv-hw';
+                          var gcse = document.createElement('script');
+                          gcse.type = 'text/javascript';
+                          gcse.async = true;
+                          gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+                          var s = document.getElementsByTagName('script')[0];
+                          s.parentNode.insertBefore(gcse, s);
+                        })();
+                      </script>
+                      <gcse:search></gcse:search>
                     </div>
                 </li>
             </ul>
